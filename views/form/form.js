37b2cc72bdc575e943830aa75cd7e433a881aa4e
Alberto Velez <nkkvlz@gmail.com>
2016-03-24 13:15:14 -0400

    Added files via upload

---
+++ views/form/form.js
@@ -0,0 +1,19 @@
+
+var main = function(){
+
+$(".drop").click(function(){
+$(".cssmenu").css("display", "block");
+$(".row1").css("height","90px")
+})
+
+$(".unsub").click(function(){
+	
+$(".unsubscribe").css("display", "block");
+})
+
+
+
+
+};
+
+$(document).ready(main);
